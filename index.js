26701cebc37e2b7264023abdebd61234d1e9541e
Sven Meyer <git_dev@svenmeyer.com>
2020-04-19 23:22:18 +1000

    added secp256k1 for dramatic speed improvement

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// https://ethereum.stackexchange.com/questions/1771/how-to-validate-a-private-key
+
 const public_address = 'af7560c61fc7d3d4404e8403b7b9194d62d7be2e'.toLowerCase();
 const private_key = '3334057ece90ab38e401718737636c4ebb8c623f82382c6aa53c309bf0d6ec62';
 const pk          = '0000007ece90ab38e401718737636c4ebb8c623f82382c6aa53c309bf0d6ec62';
@@ -7,6 +9,7 @@ if (process.argv.length == 4) {
 	console.log("parameter 2 :", process.argv[3])
 }
 
+const secp256k1 = require('secp256k1')
 const ethereumjsWallet = require('ethereumjs-wallet')
 var r
 var pk_buf = Buffer.from(pk, 'hex');
@@ -17,8 +20,8 @@ const public_address_buf = Buffer.from(public_address, 'hex');
 
 console.log("public_address_buf :", public_address_buf)
 
-
-for (let x = 50; x < 256; x++) {
+var n=0;
+for (let x = 60; x >= 0; x--) {
 	pk_buf[0] = x;
 	console.log("x =", x)
 
@@ -29,15 +32,15 @@ for (let x = 50; x < 256; x++) {
 			pk_buf[2] = z;
 
 			r = ethereumjsWallet.fromPrivateKey(pk_buf)
-			// w_address = r.getAddress().toString('hex')
-			// if (w_address == public_address) {
+			n++;
+
 			if (public_address_buf.compare(r.getAddress()) == 0) {
 				console.log("found matching addess")
 				console.log("public address :", r.getAddress().toString('hex'))
 				// console.log("Public Key     :", r.getPublicKey().toString('hex'))
 				console.log("private key    :", r.getPrivateKey().toString('hex'))
-				console.log(x * y * z , " combinations tested");
-				x = 256;
+				console.log(n , " combinations tested");
+				x =  -1;
 				y = 256;
 				z = 256;
 			}
